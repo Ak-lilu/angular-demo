809eb4e84aeaa4988000b61818a0e258b2bf3d03
Aklilu Weldemihret Weldu <akliluweldu@gmail.com>
2025-05-09 15:02:46 +0200

    ang navigation

--- src/app/UI/navigation/navigation.component.ts
+++ src/app/UI/navigation/navigation.component.ts
@@ -17,5 +17,8 @@ export class NavigationComponent {
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
+  getRandomColor() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + ('000000' + color).slice(-6);
+  }
 }
--- src/app/user/list-user/list-user.component.ts
+++ src/app/user/list-user/list-user.component.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class ListUserComponent implements OnInit {
   newsUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=e3a9ab6e98f548468d98d6283a014194";
   movie = 'https://imdb8.p.rapidapi.com/title/get-most-popular-movies?homeCountry=US&purchaseCountry=US&currentCountry=US&apiKey=4f141d8bf1msh7d3f3058f379a27p164144jsnd989bf08b98f';
-  data: any[];
+  biruck: any[];
   moviesList: any[];
 
   constructor(private http:HttpClient) { }
@@ -22,12 +22,12 @@ export class ListUserComponent implements OnInit {
 getMovies(){
   this.http.get<any[]>(this.movie).subscribe((res)=>{
     this.moviesList = res;
-    console.log(this.data)
+    //console.log(this.data)
   })
 }
   getNews() {
      return this.http.get<any[]>(this.newsUrl).subscribe( (response) => {
-       this.data = response['articles'];
+       this.biruck = response['articles'];
     //   console.log(this.data)
      });
   }
